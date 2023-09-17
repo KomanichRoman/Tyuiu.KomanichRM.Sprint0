0c57e7af691463152d8b7c2cb426085cfea5c19d
Tyuiu.KomanichRM@outlook.com <gorotro_215@mail.ru>
2023-09-17 22:52:43 +0500

    Task6

--- Tyuiu.KomanichRM.Sprint0.Task6.V0.Test/DataServiceTest.cs
+++ Tyuiu.KomanichRM.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -17,14 +17,14 @@ namespace Tyuiu.KomanichRM.Sprint0.Task6.V0.Test
         public void CheckSubstractionArrayValid()
         {
             var numbers = new int[] { 1, 2, 3, 4, 5 };
-            var res = DataService.SubstractionArray(numbers);
+            var res = DataService.SubtractionArray(numbers);
             Assert.AreEqual(-15, res);
         }
         [TestMethod]
         public void CheckMultArrayValid()
         {
             var numbers = new int[] { 1, 2, 3, 4, 5 };
-            var res = DataService.MultArray(numbers);
+            var res = DataService.MultiplicationArray(numbers);
             Assert.AreEqual(120, res);
         }
     }
